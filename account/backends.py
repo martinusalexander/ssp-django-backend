ae84394e4101376563ca6ad74bffbd64a9f60201
Martinus <martinus.alexander.looho@gmail.com>
2021-11-29 19:33:06 +0800

    Fix bug in auth backend, validate password

--- account/backends.py
+++ account/backends.py
@@ -14,7 +14,9 @@ class CustomBackend(BaseBackend):
 
         try:
             user = User.objects.get(email=email)
-            return user
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+            return None
         except User.DoesNotExist:
             return None
 
@@ -23,3 +25,11 @@ class CustomBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+    def user_can_authenticate(self, user):
+        """
+        Reject users with is_active=False.
+        Custom user models that don't have that attribute are allowed.
+        """
+        is_active = getattr(user, 'is_active', None)
+        return is_active or is_active is None
